db610714112412d51211b4eb55f6a34c77b09ae9
LittleBigWorld <b.paslavskyi-stud14317@chnu.edu.ua>
2020-07-06 21:06:24 +0300

    Module 6

--- Module 5/5_2_fib_reccurtion.rb
+++ Module 5/5_2_fib_reccurtion.rb
@@ -10,6 +10,11 @@
 
 class Calculator
   def fibonacci n
-    ...
+    if n<=1
+      n
+    else
+      fibonacci(n-1)+fibonacci(n-2)
+    end
   end
 end
+puts Calculator.new.fibonacci(gets.chomp.to_i)
